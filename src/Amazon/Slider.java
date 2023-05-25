7dd28ca30cde41d38ba2a5dbe6f233f1a44ca832
JunuThapa <junuthapa011@gmail.com>
2023-05-25 01:03:44 -0400

    Daraz Slider class and Amazon Slider class added

---
+++ src/Amazon/Slider.java
@@ -0,0 +1,19 @@
+package Amazon;
+
+public class Slider {
+    /*
+     Image URL
+     Slider ID
+     Slider URL
+     Order ID
+
+     Slider 1
+        Image URL:
+        Slider ID
+        Slider URL
+        Order ID
+
+     */
+
+
+}
---
+++ src/Daraz/Slider.java
@@ -0,0 +1,23 @@
+package Daraz;
+
+public class Slider {
+
+    /*
+        Slider ID
+        Image
+        Slider URL
+        Order ID
+
+        Slider 1
+            Slider ID: 1
+            Image: https://icms-image.slatic.net/images/ims-web/b87412ca-5e12-4e86-9b51-5f3dc1354337.jpg_1200x1200.jpg
+            Slider URL: https://pages.daraz.com.np/wow/gcp/daraz/megascenario/np/jesthkobest/jkb-live?spm=a2a0e.11779170.top.dbr1.287d2d2bCBGEoj&pfilter=1010Badge_273165&scm=1003.4.icms-zebra-100031662-2974963.OTHER_6502391762_7714641
+            Order ID: 1
+
+       Slider 2
+            Slider ID: 2
+            Image: https://icms-image.slatic.net/images/ims-web/791745fc-3394-4712-8feb-86b699813f13.jpg
+            Slider URL: https://www.daraz.com.np/shop/brillare-science-india?spm=a2a0e.11779170.top.dbr2.287d2d2bNOqwsx&from=onesearch_brand_124043833&scm=1003.4.icms-zebra-100031662-2974963.OTHER_6502391763_7714641
+            Order ID: 2
+     */
+}
