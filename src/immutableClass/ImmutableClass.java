59afe21b664e6204bcf7614756ca30385ac0121e
JunuThapa <junuthapa011@gmail.com>
2023-06-15 02:57:16 -0400

    added Immmutable class

---
+++ src/immutableClass/ImmutableClass.java
@@ -0,0 +1,40 @@
+package immutableClass;
+
+final class ImmutableClass {
+
+    //initializing states or variables
+    private final long id;
+    private final String name;
+    private final int rollNum;
+
+    //parameterized constructor
+    public ImmutableClass(long id, String name, int rollNum){
+        this.id=id;
+        this.name=name;
+        this.rollNum=rollNum;
+    }
+
+    //Getter method
+
+    public long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getRollNum(){
+        return rollNum;
+    }
+
+    @Override
+    public String toString() {
+        return "ImmutableClass{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", rollNum=" + rollNum +
+                '}';
+    }
+}
+
